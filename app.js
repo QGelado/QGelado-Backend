fc9fa229a008e4d1fc308ecce78f5d856d9bdcd9
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-02-24 13:28:40 -0300

    firs commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import "dotenv/config"
+import express from "express"
+import connectDatabase from "./src/config/database.js"
+import routes from "./src/routes/index.js"
+
+const connection = await connectDatabase();
+
+connection.on("error", (erro) => {
+    console.error(`Falha ao conectar com o banco de dados: ${erro}`)
+});
+
+connection.once("open", () => {
+    console.log("A conexão foi feita com sucesso!")
+});
+
+const app = express();
+
+routes(app);
+
+const port = 3000;
+
+app.listen(port, () => console.log("Servidor online em http://localhost:3000/"))
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const routes = app => {
+    app.use(express.json());
+}
+
+export default routes;
