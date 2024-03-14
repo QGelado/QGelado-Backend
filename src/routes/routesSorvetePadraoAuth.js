a5d053929966cb57b86171a826a3f9e114837959
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-03-14 17:12:28 -0300

    :recycle: Rotas com autenticação

--- src/routes/routesSorvetePadraoAuth.js
+++ src/routes/routesSorvetePadraoAuth.js
@@ -4,8 +4,8 @@ import upload from "../config/multer.js"
 
 const routesSorvetePadraoAuth = express.Router();
 
-routesSorvetePadrao.post("/sorvete-padrao", upload('sorvete-padrao').single('file'), SorvetePadraoController.cadastraSorvete)
-routesSorvetePadrao.put("/sorvete-padrao/:id", upload('sorvete-padrao').single('file'), SorvetePadraoController.atualizaSorvete)
-routesSorvetePadrao.delete("/sorvete-padrao/:id", SorvetePadraoController.deletaSorvete)
+routesSorvetePadraoAuth.post("/sorvete-padrao", upload('sorvete-padrao').single('file'), SorvetePadraoController.cadastraSorvete)
+routesSorvetePadraoAuth.put("/sorvete-padrao/:id", upload('sorvete-padrao').single('file'), SorvetePadraoController.atualizaSorvete)
+routesSorvetePadraoAuth.delete("/sorvete-padrao/:id", SorvetePadraoController.deletaSorvete)
 
 export default routesSorvetePadraoAuth;
