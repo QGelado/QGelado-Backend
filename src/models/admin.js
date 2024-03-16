6085a4fc1aef681479494167d9b945d84df96be9
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2024-03-16 16:32:15 -0300

    :safety_vest: validation
    - add a rule that only allows strong passwords to the admins

--- src/models/admin.js
+++ src/models/admin.js
@@ -4,7 +4,13 @@ const adminSchema = new mongoose.Schema({
     id: {type: mongoose.Schema.Types.ObjectId},
     nome: {type: mongoose.Schema.Types.String, required: [true, "O nome é obrigatória"]},
     email: {type: mongoose.Schema.Types.String, required: [true, "O email é obrigatória"]},
-    senha: {type: mongoose.Schema.Types.String, required: [true, "A senha é obrigatória"]},
+    senha: {type: mongoose.Schema.Types.String, validate: {
+        validator: value => {
+            const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{20,}$/;
+            return (!value || !value.trim().length) || re.test(value)
+        },
+        message: "Sua senha precisa ter pelo menos um dígito, uma letra minuscula e uma maiúscula e no minimo 20 caracteres!"
+    }},
 }, {versionKey: false});
 
 const adminModel = mongoose.model("admins", adminSchema);
