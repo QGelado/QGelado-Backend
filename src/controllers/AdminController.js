633a288a02451ec715af16787eeccce8bf6a8080
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2024-03-16 16:19:16 -0300

    :goal_net: erros
    - add erros messages

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,64 +1,78 @@
 import { adminModel } from "../models/admin.js";
+import NotFoundError from "../erros/NotFoundError.js"
 
 class AdminController{
 
-    static async buscaTodosOsAdmin(req, res){
+    static async buscaTodosOsAdmin(req, res, next){
         try {
             const resultadoAdmin = await adminModel.find({});
 
             res.status(200).json(resultadoAdmin);
         } catch (error) {
-            res.status(500).json({message: `Falha na requisição ${error.message}`});
+            next(error);
         }
     }
 
-    static async buscaUmAdmin(req, res){
+    static async buscaUmAdmin(req, res, next){
         try {
             const idAdmin = req.params.id;
             const resultadoAdmin = await adminModel.findById(idAdmin);
 
-            res.status(200).json(resultadoAdmin);
+            if(resultadoAdmin !== null){
+                res.status(200).json(resultadoAdmin);
+            }else{
+                next(new NotFoundError("ID do admin não encontrado!"));
+            }
+
         } catch (error) {
-            res.status(500).json({message: `Falha na requisição ${error.message}` });
+            next(error);
         }
     }
 
-    static async criaUmNovoAdmin(req, res){
+    static async criaUmNovoAdmin(req, res, next){
         try {
             const dadosAdmin = req.body;
             const retornoAdminCriado = await adminModel.create(dadosAdmin);
 
             res.status(201).json({message: "Admin criado!", data: retornoAdminCriado});
         } catch (error) {
-            res.status(500).json({message: `Falha na requisição ${error.message}`});
+            next(error);
         }
     }
 
-    static async atualizaUmAdmin(req, res){
+    static async atualizaUmAdmin(req, res, next){
         try {
             const idAdmin = req.params.id;
             const dadosAdmin = req.body;
 
             await adminModel.findByIdAndUpdate(idAdmin, dadosAdmin);
             const retornoAtualizacao = await adminModel.findById(idAdmin);
+            
+            if (retornoAtualizacao !== null) {
+                res.status(200).json({message: "Admin atualizado!", data: retornoAtualizacao});
+            }else{
+                next(new NotFoundError("ID do admin não encontrado!"));
+            }
 
-            res.status(200).json({message: "Admin atualizado!", data: retornoAtualizacao});
         } catch (error) {
-            res.status(500).json({message: `Falha na requisição ${error.message}`});
+            next(error);
         }
     }
 
-    static async deletaUmAdmin(req, res){
+    static async deletaUmAdmin(req, res, next){
         try {
             const idAdmin = req.params.id;
-            await adminModel.findByIdAndDelete(idAdmin);
-
-            res.status(200).json({message: "Admin deletado com sucesso!"})
+            const respostaDelecao = await adminModel.findByIdAndDelete(idAdmin);
+            
+            if (respostaDelecao !== null) {
+                res.status(200).json({message: "Admin deletado com sucesso!"})                
+            } else {
+                next(new NotFoundError("ID do admin não encontrado!"));
+            } 
         } catch (error) {
-            res.status(500).json({message: `Falha na requisição ${error.message}`});
+            next(error);
         }
     }
-
 }
 
 export default AdminController;
